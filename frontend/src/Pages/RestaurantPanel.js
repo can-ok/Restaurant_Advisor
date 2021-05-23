cc1c71366b7bec46986d64ff35b5a63c42306719
can-ok <canokan.taskiran@ogr.iu.edu.tr>
2021-05-24 00:16:41 +0300

    image uploaders added

--- frontend/src/Pages/RestaurantPanel.js
+++ frontend/src/Pages/RestaurantPanel.js
@@ -12,12 +12,15 @@ import {
 import Select from "react-select";
 import ReactStars from "react-rating-stars-component";
 
+import sample from "../assets/images/sample.jpg";
 
 const RestaurantPanel = () => {
   const [restaurantName, setRestaurant] = useState("");
   const [genre, setGenre] = useState("");
   const [side, setSide] = useState("");
   const [rating, setRating] = useState(0);
+  const [selectedMenu, setMenu] = useState(sample);
+  const [selectedPhoto, setPhoto] = useState(sample);
 
   const genreOptions = [
     { value: "Fish and Seafood", label: "Fish and Seafood" },
@@ -32,10 +35,23 @@ const RestaurantPanel = () => {
     { value: "Avrupa", label: "Avrupa" },
   ];
 
+  const imageHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        if (e.target.name == "menu") setMenu(reader.result);
+        else setPhoto(reader.result);
+
+        console.log(reader.result)
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
   return (
-    <div>
+    <div className="restaurant-panel">
       <Form>
-        <FormGroup>
+        <FormGroup className="input-row">
           <div className="row">
             <div className="col-sm-2">
               <Label for="restaurantName">Restaurant Name:</Label>
@@ -53,7 +69,7 @@ const RestaurantPanel = () => {
           </div>
         </FormGroup>
 
-        <FormGroup>
+        <FormGroup className="input-row">
           <div className="row">
             <div className="col-sm">
               <div className="row">
@@ -77,34 +93,38 @@ const RestaurantPanel = () => {
                 </div>
 
                 <div className="col-sm-8">
-                  <Select options={sideOptions} onChange={setSide} isClearable />
+                  <Select
+                    options={sideOptions}
+                    onChange={setSide}
+                    isClearable
+                  />
                 </div>
               </div>
             </div>
           </div>
         </FormGroup>
 
-        <FormGroup>
+        <FormGroup className="input-row">
           <div className="row">
             <div className="col-sm-2">
               <Label for="restRating">Rating:</Label>
             </div>
             <div className="col-sm-9">
-            <ReactStars
-              count={5}
-              onChange={setRating}
-              size={50}
-              isHalf={true}
-              emptyIcon={<i className="far fa-star"></i>}
-              halfIcon={<i className="fa fa-star-half-alt"></i>}
-              fullIcon={<i className="fa fa-star"></i>}
-              activeColor="#ffd700"
-            />
+              <ReactStars
+                count={5}
+                onChange={setRating}
+                size={50}
+                isHalf={true}
+                emptyIcon={<i className="far fa-star"></i>}
+                halfIcon={<i className="fa fa-star-half-alt"></i>}
+                fullIcon={<i className="fa fa-star"></i>}
+                activeColor="#ffd700"
+              />
             </div>
           </div>
         </FormGroup>
 
-        <FormGroup>
+        <FormGroup className="input-row">
           <div className="row">
             <div className="col-sm-2">
               <Label for="restaurantDescription">Description:</Label>
@@ -112,6 +132,55 @@ const RestaurantPanel = () => {
             <div className="col-sm-9">
               <Input type="textarea" name="text" id="restaurantDescription" />
             </div>
+          </div>
+        </FormGroup>
+
+        <FormGroup className="input-row">
+          <div className="row">
+            <div className="col-sm photo-selecter">
+              <div className="row ">
+                <Label className="photo-header">Menu:</Label>
+              </div>
+
+              <div className="row">
+                <input
+                  type="file"
+                  name="menu"
+                  id="input"
+                  accept="image/*"
+                  onChange={imageHandler}
+                />
+
+                <img src={selectedMenu} alt="menu" id="img" className="img" />
+              </div>
+            </div>
+
+            <div className="col-sm  photo-selecter">
+              <div className="row">
+                <Label className="photo-header">Photograph:</Label>
+              </div>
+              
+              <div className="row">
+
+                <input
+                  type="file"
+                  name="restaurant-photo"
+                  id="input"
+                  accept="image/*"
+                  onChange={imageHandler}
+                />
+
+                <img src={selectedPhoto} alt="photo" id="img" className="img" />
+              </div>
+
+              </div>
+          </div>
+        </FormGroup>
+
+        <FormGroup class="input-row">
+          <div className="submit-row">
+            <Button color="success" className="btn" type="submit">Submit</Button>
+            <Button color="danger" className="btn" type="submit">Cancel</Button>
           </div>
         </FormGroup>
       </Form>
